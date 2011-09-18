894d9930c00090cb89b22a3f20916ca6666571a1
Igor Suvorov <>
2011-09-19 01:17:39 +0400

    Загрузка страниц сайта

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   for ( $id = 1; $id <= $maxId; $id++ ) {
       $filenames[] = "http://animalzone.ru/articles2/{$id}/";
   }
+  
   foreach ( $filenames as $filename ) {
       echo $filename;
       
